5521fcd57307a9fc8fb853ae4767878c765584a5
Ivan Nakatani <ivan.nakatani@gmail.com>
2024-03-06 20:25:42 -0300

    configuracao database

--- GuruGourmet/settings.py
+++ GuruGourmet/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'GuruGourmet.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'postgres',
+       'USER': 'postgres.xnvsgzbluxytgkvjpzcy',
+       'PASSWORD': 'x5D2VgXvjAPoc3j6',
+       'HOST': 'aws-0-sa-east-1.pooler.supabase.com',
+       'PORT': '5432',
+   }
 }
 
 
