8948392617d67641f1b0b1acd487ee32795cf98e
vinicius.avellar <viniavellar@gmail.com>
2024-05-22 21:57:50 -0300

    push

--- core/models.py
+++ core/models.py
@@ -9,6 +9,8 @@ class Usuario(models.Model):
     senha = models.CharField()
     cnpj = models.CharField(null=True)
     userDjango = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
+    salarioMinimo = models.FloatField(default=0.0)
+
 
 class Endereco(models.Model):
     rua = models.CharField(max_length=255)
