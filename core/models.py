20652dbe27f6d658afd8312410909e7c5c87a680
Ivan Nakatani <ivan.nakatani@gmail.com>
2024-05-29 14:54:06 -0300

    ajustes

--- core/models.py
+++ core/models.py
@@ -9,6 +9,7 @@ class Usuario(models.Model):
     senha = models.CharField()
     userDjango = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
     is_admin = models.BooleanField(default=False)
+    status = models.BooleanField(default=False)
 
 class Endereco(models.Model):
     rua = models.CharField(max_length=255)
